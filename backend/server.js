ab863f6e7b12c1e5fc715f3ce73673523a51acdf
Justin <justin_jang@brown.edu>
2024-07-10 21:27:28 -0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const { chats } = require("./data/data")
+const dotenv = require("dotenv")
+
+const app = express()
+dotenv.config();
+
+app.get('/', (req,res) => {
+    res.send("API is Running Successfully");
+
+});
+
+app.get('/api/chat', (req,res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req,res) => {
+    // console.log(req.params.id)
+    const singleChat = chats.find(c => c._id === req.params.id)
+    res.send(singleChat)
+})
+
+const PORT = process.env.PORT 
+
+app.listen(5001,console.log(`Server started on PORT ${PORT}`));

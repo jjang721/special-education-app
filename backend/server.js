afdca1c36dadb4f29e9f17fb87b51902919b29f5
Justin <justin_jang@brown.edu>
2024-07-13 15:33:23 -0700

    mongo

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,29 @@
 const express = require("express")
 const { chats } = require("./data/data")
 const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+const userRoutes = require("./routes/userRoutes")
 
 const app = express()
 dotenv.config();
+connectDB()
 
 app.get('/', (req,res) => {
     res.send("API is Running Successfully");
 
 });
 
-app.get('/api/chat', (req,res) => {
-    res.send(chats)
-})
+// app.get('/api/chat', (req,res) => {
+//     res.send(chats)
+// })
 
-app.get('/api/chat/:id', (req,res) => {
-    // console.log(req.params.id)
-    const singleChat = chats.find(c => c._id === req.params.id)
-    res.send(singleChat)
-})
+// app.get('/api/chat/:id', (req,res) => {
+//     // console.log(req.params.id)
+//     const singleChat = chats.find(c => c._id === req.params.id)
+//     res.send(singleChat)
+// })
+
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT 
 

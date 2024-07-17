ba7992ecad1bd5cb3a50f8800cea29a177bff998
Justin <justin_jang@brown.edu>
2024-07-16 17:39:20 -0700

    authent

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,22 @@ const { chats } = require("./data/data")
 const dotenv = require("dotenv")
 const connectDB = require("./config/db")
 const userRoutes = require("./routes/userRoutes")
+const {notFound,errorHandler} = require('./middleware/errorMiddleware')
 
 const app = express()
 dotenv.config();
 connectDB()
+
+app.use(express.json()) //this accepts the json data
 
 app.get('/', (req,res) => {
     res.send("API is Running Successfully");
 
 });
 
-// app.get('/api/chat', (req,res) => {
-//     res.send(chats)
-// })
+app.get('/api/chat', (req,res) => {
+    res.send(chats)
+})
 
 // app.get('/api/chat/:id', (req,res) => {
 //     // console.log(req.params.id)
@@ -24,6 +27,9 @@ app.get('/', (req,res) => {
 // })
 
 app.use('/api/user', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT 
 

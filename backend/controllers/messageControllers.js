0f75e98499256894678fd4ce43247ec1cb95e083
Justin <justin_jang@brown.edu>
2024-08-03 17:21:44 -0700

    messages

---
+++ backend/controllers/messageControllers.js
@@ -0,0 +1,19 @@
+const expressAsyncHandler = require("express-async-handler");
+
+const sendMessage = expressAsyncHandler(async(req,res) => {
+    const { content, chatId } = req.body
+
+    if (!content || !chatId) {
+        console.log("Invalid data passed into request");
+        return res.sendStatus(400);
+    }
+
+    var newMessage = {
+        sender: req.user._id,
+        content: content,
+        chat: chatId,
+    }
+
+})
+
+module.exports = { sendMessage }
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv")
 const connectDB = require("./config/db")
 const userRoutes = require("./routes/userRoutes")
 const chatRoutes = require("./routes/chatRoutes")
+const messageRoutes = require("./routes/messageRoutes")
 const {notFound,errorHandler} = require('./middleware/errorMiddleware')
 
 const app = express()
@@ -29,6 +30,7 @@ app.get('/api/chat', (req,res) => {
 
 app.use('/api/user', userRoutes)
 app.use('/api/chat', chatRoutes)
+app.use('/api/message', messageRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
